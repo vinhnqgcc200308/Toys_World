3763001fe07d6e56f0eca1cc4c5b6703c16ff8b2
Tuan998 <100896109+Tuan998@users.noreply.github.com>
2022-07-11 08:05:01 +0700

    Create CartItemType.php
    7/11/2022

---
+++ src/Form/CartItemType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\OrderItem;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class CartItemType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            #->add('date')
+            #->add('deliverydate')
+            ->add('quantity')
+            ->add('customer')
+            ->add('remove', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => OrderItem::class,
+        ]);
+    }
+}
